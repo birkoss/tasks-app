87d859feafa6e1ad9fa7c7ab7a635bd69d5d20d7
Mathieu Robichaud <mathieu@standish.ca>
2020-06-03 15:39:45 -0400

    Refactor

--- App.tsx
+++ App.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    TouchableHighlight,
+    SafeAreaView,
+    Button,
+    Alert,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    let x = 1;
+    console.log("asasassa");
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Testing 1234</Text>
+            <Button
+                color="orange"
+                title="Click me"
+                onPress={() =>
+                    Alert.alert("TItre", "Message", [
+                        {
+                            text: "Yes",
+                            onPress: () => console.log("YES"),
+                        },
+                        {
+                            text: "Nop",
+                            onPress: () => console.log("NOP"),
+                        },
+                    ])
+                }
+            />
+            <Image source={require("./app/assets/icon.png")} />
+            <TouchableHighlight onPress={() => console.log("Image tapped")}>
+                <Image
+                    source={{
+                        width: 200,
+                        height: 300,
+                        uri: "https://picsum.photos/200/300",
+                    }}
+                />
+            </TouchableHighlight>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
